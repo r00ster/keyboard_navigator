de45a30a0c3d7f7ae8f4e5e8a61d2eef27f735ca
niel <niel@geckotech.nl>
2016-01-28 08:32:57 +0200

    Updated comments

--- js/jquery.keyboardNavigator.js
+++ js/jquery.keyboardNavigator.js
@@ -4,11 +4,12 @@
         var idx = 0;
         var distance;
 
-        // Allow moving up/down between elements from current scroll position 
+        // allow moving up/down between elements from current scroll position 
         $(window).bind('scroll', function() {
             $elements.each(function() {
                 var $elem = $(this);
                 distance = $elem.offset().top - $(window).scrollTop();
+                // break out of loop when 'topmost' element is found
                 if(distance >= 0) {
                     idx = $elements.index($elem);
                     return false;
@@ -18,7 +19,7 @@
 
         $(document).keydown(function(e) {
             if (e.keyCode == '37' || e.keyCode == '75') {
-                // left arrow
+                // left arrow or the letter 'k'
                 e.preventDefault();
                 if(idx > 0) {
                     var $prev = $elements.eq(idx - 1);
@@ -26,9 +27,10 @@
                     $(window).scrollTop(topOffset);
                 }
             } else if (e.keyCode == '39' || e.keyCode == '74') {
-                // right arrow
+                // right arrow or the letter 'j'
                 e.preventDefault();
                 if(idx < ($elements.length)) {
+                    // check if element is at top of page
                     var nextPos = distance < 1 ? 1 : 0;
                     var $next = $elements.eq(idx + nextPos);
                     var topOffset = $next.offset().top;
